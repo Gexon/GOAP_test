fa5c5561b48cfade96cfd6ced5dd8391a17d7c79
Gexon <alimgan@gmail.com>
2017-11-16 17:54:12 +0500

    переписал на другой крейт gpgoal. все отлично работает!

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,65 @@
-extern crate rgoap;
+extern crate gpgoap;
 
-use rgoap::{State, Action, plan};
+use gpgoap::{WorldState, ActionPlanner, AStar};
 
 
 fn main() {
-    println!("Hello, GOAP!");
+    println!("Привет, GOAP!");
 
     // Действия использующиеся в планировщике.
-    let mut walk_to_dog = Action::new("Идем к собакену".to_string(), 1);
-    walk_to_dog.pre_conditions.insert("человек с собакой".to_string(), true);
-    walk_to_dog.post_conditions.insert("рядом с собакой".to_string(), true);
+    let mut ap = ActionPlanner::new();
+    ap.set_pre("набить брюхо", "желудок урчит", true);
+    ap.set_pre("набить брюхо", "еда перед аборигеном", true);
+    ap.set_post("набить брюхо", "наелся от пуза", true);
+    ap.set_post("набить брюхо", "еда перед аборигеном", false);
 
-    let mut dog_wiggles_tail = Action::new("Псина виляет хвостом".to_string(), 1);
-    dog_wiggles_tail.pre_conditions.insert("пес счастлив".to_string(), true);
-    dog_wiggles_tail.post_conditions.insert("Хвост колышется".to_string(), true);
+    ap.set_post("помолиться о еде", "еда перед аборигеном", true);
 
-    let mut pet_dog = Action::new("Погладить псину".to_string(), 1);
-    pet_dog.pre_conditions.insert("рядом с собакой".to_string(), true);
-    pet_dog.post_conditions.insert("пес счастлив".to_string(), true);
+    ap.set_pre("убить краба", "рядом краб", true);
+    ap.set_pre("убить краба", "камень зажат в руке", true);
+    ap.set_post("убить краба", "еда перед аборигеном", true);
 
-    let mut kill_dog = Action::new("Выстрелить в псину".to_string(), 1);
-    kill_dog.pre_conditions.insert("ствол есть".to_string(), true);
-    kill_dog.post_conditions.insert("пес мертв".to_string(), true);
+    ap.set_post("поиск крабов", "рядом краб", true);
 
-    let mut find_weapon = Action::new("Найти ствол".to_string(), 1);
-    find_weapon.pre_conditions.insert("ствол есть".to_string(), false);
-    find_weapon.post_conditions.insert("ствол есть".to_string(), true);
+    ap.set_post("поиск камня", "камень зажат в руке", true);
 
-    let possible_actions = [walk_to_dog, pet_dog, dog_wiggles_tail, kill_dog, find_weapon];
+    ap.set_pre("выменять еды у товарища", "рядом другой абориген", true);
+    ap.set_pre("выменять еды у товарища", "кость зажата в руке", true);
+    ap.set_post("выменять еды у товарища", "еда перед аборигеном", true);
+    ap.set_post("выменять еды у товарища", "кость зажата в руке", false);
 
-    // Это начальное состояние мира.
-    let mut initial_state = State::new();
-    initial_state.insert("рядом с собакой".to_string(), false);
-    initial_state.insert("человек с собакой".to_string(), true);
-    initial_state.insert("пес счастлив".to_string(), false);
-    initial_state.insert("Хвост колышется".to_string(), false);
-    initial_state.insert("пес мертв".to_string(), false);
-    initial_state.insert("ствол есть".to_string(), false);
+    ap.set_post("поиск товарища", "рядом другой абориген", true);
 
-    // А это, целевое состояние. Не должен включать все состояния.
-    let mut goal_state = State::new();
-    goal_state.insert("пес мертв".to_string(), true);
+    ap.set_post("поиск косточки", "кость зажата в руке", true);
 
-    // Давайте найдем, какие действия нужно сделать, чтобы попасть туда.
-    let planned_actions = plan(&initial_state, &goal_state, &possible_actions).unwrap();
 
-    // Действия то, что мы ожидали?
-    let planned_actions_names: Vec<String> =
-        planned_actions.iter().map(|&action| action.name.clone()).collect();
-    //let expected_actions_names =
-    //    vec!["walk_to_dog".to_string(), "pet_dog".to_string(), "dog_wiggles_tail".to_string()];
-    //assert_eq!(planned_actions_names, expected_actions_names);
-    println!("Цель планировщика: {:?}", goal_state);
-    println!("Цепь действий: {:?}", planned_actions_names);
+    ap.set_cost("поиск крабов", 2);
+    ap.set_cost("убить краба", 5);
+    ap.set_cost("поиск камня", 3);
+    ap.set_cost("поиск косточки", 9);
+    ap.set_cost("помолиться о еде", 11);
+
+    //print!("Planner:\n {:?}", &ap);
+
+    let mut fr = WorldState::new();
+    fr.set(&mut ap, "желудок урчит", true);
+    fr.set(&mut ap, "alive", true);
+
+    //print!("From: \n{:?}", fr.debug_fmt(&ap));
+
+    let mut goal = WorldState::new();
+    goal.set(&mut ap, "наелся от пуза", true);
+    goal.set(&mut ap, "alive", true);
+
+    //print!("Goal: \n{:?}", goal.debug_fmt(&ap));
+
+    let mut astar = AStar::new();
+    if let Some(plan) = astar.plan(&ap, &fr, &goal) {
+        println!("Plan Cost: {}", plan.cost());
+        for (i, &(plan, result_state)) in plan.iter().enumerate() {
+            println!("{}: {}\n{:?}", i, plan, result_state.debug_fmt(&ap));
+        }
+    }
 }
 
 
